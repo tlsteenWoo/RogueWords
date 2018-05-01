a654da3f129d903680d1b1d33abbe212a150dfb3
Tim Steen <tlawrencesteen@gmail.com>
2018-05-01 08:26:52 -0700

    move screen initialize from main menu to rwgame

--- RogueWordsBase/Rogue Words/RogueWordsGame.cs
+++ RogueWordsBase/Rogue Words/RogueWordsGame.cs
@@ -70,6 +70,45 @@ namespace MknGames
 
         protected override void LoadContent()
         {
+            //initialize
+#if false
+            //viewportFull = new Viewport(0,0,395, 702);
+            //game1.graphics.PreferredBackBufferWidth = viewportFull.Width;
+            //game1.graphics.PreferredBackBufferHeight = viewportFull.Height;
+            game1.Window.AllowUserResizing = true;
+            //game1.graphics.PreferredBackBufferWidth = 395;
+            //game1.graphics.PreferredBackBufferHeight = 702;
+            //game1.graphics.PreferredBackBufferWidth = 1200;
+            //game1.graphics.PreferredBackBufferHeight = 2500;
+            int pbbw = 395;
+            int pbbh = 702;
+            game1.graphics.IsFullScreen = false;
+            {
+                float desiredHeight = GraphicsDevice.DisplayMode.TitleSafeArea.Height - 100;
+                //galaxy s6: 1440 x 2560
+                //nexus 10: 2560 x 1600
+                //64.5" sony KD-65ZD9: 3840x2160
+                //Tagital t6 watch: 240x240
+                //float resX = 1440;
+                //float resY = 2560;
+                float resX = 2560;
+                float resY = 1600;
+                float ratio = Math.Min(1, desiredHeight / resY);
+                pbbw = (int)(resX * ratio);
+                pbbh = (int)(resY * ratio);
+            }
+            game1.graphics.PreferredBackBufferHeight = pbbh;
+            game1.graphics.PreferredBackBufferWidth = pbbw;
+            game1.IsMouseVisible = true;
+#else
+            game1.graphics.IsFullScreen = true;
+            //graphics.PreferredBackBufferWidth = 800;
+            //graphics.PreferredBackBufferHeight = 480;
+            game1.graphics.SupportedOrientations = DisplayOrientation.LandscapeLeft | DisplayOrientation.LandscapeRight | DisplayOrientation.Portrait;
+            game1.graphics.PreferredBackBufferWidth = GraphicsDevice.DisplayMode.TitleSafeArea.Width;
+            game1.graphics.PreferredBackBufferHeight = GraphicsDevice.DisplayMode.TitleSafeArea.Height;
+#endif
+            game1.graphics.ApplyChanges();
             base.LoadContent();
             //game1.graphics.PreferredBackBufferWidth = 425;
             //game1.graphics.PreferredBackBufferHeight = 450;
